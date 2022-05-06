d739ea4fd23181ebfa5b1b5a2707a606bfd25f85
Chineme <chineme2101@gmail.com>
2022-05-06 15:20:35 +0100

    Updating a poorly executed assignment

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var year = document.getElementById("years").value;
+
+    document.getElementById("Output").innerHTML = "If you deposit " + principal + "," + "<br>"
+                                                + "at an interest rate of " + rate + "." + "<br>"
+                                                + "You will recieve an amount of " + (Number(principal) + (principal*rate*year)/100) + "," + "<br>"
+                                                + "in the year " + (Number(year) + new Date().getFullYear()) + "." + "<br>";
 }
-        
